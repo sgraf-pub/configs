09212a6ee834808496e14811729b3c9fbdc7aa39
Stanislav Graf <stanislav.graf@gmail.com>
2016-10-12 11:30:39 +0200

    Change monitor setup

--- roles/lxde-monitors/files/set-monitors.sh
+++ roles/lxde-monitors/files/set-monitors.sh
@@ -3,17 +3,17 @@
 devices_n=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | wc -l)
 
 if [ ${devices_n} -gt 1 ] ; then
-    xrandr --output DP1   --mode 1920x1080 --pos 1680x-870 --rotate left \
-           --output DP2-1 --mode 1920x1080 --pos 2760x-870 --rotate left \
-           --output DP2-2 --mode 1680x1050 --pos 0x0       --rotate normal \
-           --output eDP1  --off \
-           --output DP2   --off \
-           --output DP2-3 --off \
-           --output HDMI1 --off \
-           --output HDMI2 --off \
+    xrandr --output DP2-2    --auto \
+           --output DP1      --auto --right-of DP2-2 \
+           --output DP2-1    --auto --right-of DP1 \
+           --output DP2      --auto \
+           --output DP2-3    --auto \
+           --output HDMI1    --auto \
+           --output HDMI2    --auto \
+           --output eDP1     --off  \
            --output VIRTUAL1 --off
 else
-    xrandr --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal \
+    xrandr --output eDP1 --auto \
            --output DP1 --off --output DP2-1 --off --output DP2-2 --off \
            --output DP2 --off --output DP2-3 --off --output HDMI1 --off \
            --output HDMI2 --off --output VIRTUAL1 --off
