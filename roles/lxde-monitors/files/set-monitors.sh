6599c6e083a0fb6ff0856deb53fc04e2881af3d1
Stanislav Graf <stanislav.graf@gmail.com>
2016-11-05 15:22:26 +0100

    Change monitor settings

--- roles/lxde-monitors/files/set-monitors.sh
+++ roles/lxde-monitors/files/set-monitors.sh
@@ -2,16 +2,11 @@
 
 devices_n=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/" | wc -l)
 
-if [ ${devices_n} -gt 1 ] ; then
+if [ ${devices_n} -eq 4 ] ; then
     xrandr --output DP2-2    --auto \
            --output DP1      --auto --right-of DP2-2 \
            --output DP2-1    --auto --right-of DP1 \
-           --output DP2      --auto \
-           --output DP2-3    --auto \
-           --output HDMI1    --auto \
-           --output HDMI2    --auto \
-           --output eDP1     --off  \
-           --output VIRTUAL1 --off
+           --output eDP1     --off
 else
     xrandr --output eDP1 --auto \
            --output DP1 --off --output DP2-1 --off --output DP2-2 --off \
