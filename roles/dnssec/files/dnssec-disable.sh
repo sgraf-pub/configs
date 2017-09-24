147f34151b75e819472bf5352e6a390a42793258
Stanislav Graf <stanislav.graf@gmail.com>
2017-09-24 10:53:47 +0200

    Disabling unbound should enable dnsmasq

--- roles/dnssec/files/dnssec-disable.sh
+++ roles/dnssec/files/dnssec-disable.sh
@@ -3,6 +3,6 @@
 /usr/bin/systemctl disable dnssec-triggerd
 /usr/bin/systemctl stop dnssec-triggerd
 /usr/bin/systemctl stop unbound
-/usr/bin/sed -i 's/^dns/#dns/' /etc/NetworkManager/NetworkManager.conf
+/usr/bin/sed -i 's/^dns.*/dns = dnsmasq/' /etc/NetworkManager/NetworkManager.conf
 /usr/bin/systemctl restart NetworkManager
 
