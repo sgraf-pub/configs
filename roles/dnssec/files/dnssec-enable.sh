2f9ce64008eb62039b1497bf4cc299e86c044fe7
Stanislav Graf <stanislav.graf@gmail.com>
2017-09-24 11:22:31 +0200

    Fix sed for enabling dnssec

--- roles/dnssec/files/dnssec-enable.sh
+++ roles/dnssec/files/dnssec-enable.sh
@@ -2,6 +2,6 @@
 
 /usr/bin/systemctl enable dnssec-triggerd
 /usr/bin/systemctl start dnssec-triggerd
-/usr/bin/sed -i 's/^#dns/dns/' /etc/NetworkManager/NetworkManager.conf
+/usr/bin/sed -i 's/^dns.*/dns = unbound/' /etc/NetworkManager/NetworkManager.conf
 /usr/bin/systemctl restart NetworkManager
 
