57016731bb85097859d96f92d2b58853b42e7215
Stanislav Graf <stanislav.graf@gmail.com>
2017-12-23 15:59:22 +0100

    Add coin mining sites to blacklists
    Plus code refactoring.

--- roles/hosts/files/parse_hosts_file.py
+++ roles/hosts/files/parse_hosts_file.py
@@ -14,26 +14,30 @@ def run(cmd):
 
 def main():
     # get only hostnames
-    hosts_list = [host for host in run("curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts | grep '^0\.0\.0\.0' | awk '{ print $2 }' | rev | sort | uniq").split('\n') if host]
+    hosts_list = [host for host in run("curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt | grep '^0\.0\.0\.0' | awk '{ print $2 }' | rev | sort | uniq").split('\n') if host]
 
     # keep only parrent domains
     for index in xrange(len(hosts_list)):
-        for host in hosts_list[index+1:]:
-            try:
-                if host.startswith('%s' % hosts_list[index]):
-                    if host.startswith('%s.' % hosts_list[index]):
-                        hosts_list.remove(host)
+        try:
+            current_host = hosts_list[index]
+            subdomains = []
+            for subdomain in hosts_list[index+1:]:
+                if subdomain.startswith('%s' % current_host):
+                    if subdomain.startswith('%s.' % current_host):
+                        subdomains += [subdomain]
                 else:
                     break
-            except IndexError:
-                break
+        except IndexError:
+            break
+
+        for host in subdomains:
+            hosts_list.remove(host)
 
     # undo rev
     hosts_list = [host[::-1] for host in hosts_list]
 
     with open("/etc/unbound/local.d/ads.conf", "w") as text_file:
         for host in hosts_list:
-            #text_file.write("address=/%s/\n" % host)
             text_file.write('local-zone: "%s" always_nxdomain\n' % host)
 
     with open("/etc/NetworkManager/dnsmasq.d/ads.conf", "w") as text_file:
