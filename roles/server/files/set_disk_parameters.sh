b3a5b3aade0b615b575b91e9a7918db522a96b25
Stanislav Graf <stanislav.graf@gmail.com>
2018-07-15 12:45:25 +0200

    Add full path

--- roles/server/files/set_disk_parameters.sh
+++ roles/server/files/set_disk_parameters.sh
@@ -1,19 +1,19 @@
 #/bin/bash -x
 
 for disk in /dev/sd?; do
-    if  smartctl --quietmode errorsonly \
-                 --info \
-                 --nocheck standby \
-                 $disk &>/dev/null ; then
+    if  /usr/sbin/smartctl  --quietmode errorsonly \
+                            --info \
+                            --nocheck standby \
+                            $disk &>/dev/null ; then
 
         echo $disk
-        smartctl --set aam,off \
-                 --set apm,128 \
-                 --set lookahead,on \
-                 --set wcache,off \
-                 --set wcreorder,off \
-                 --set standby,241 \
-                 $disk
+        /usr/sbin/smartctl  --set aam,off \
+                            --set apm,128 \
+                            --set lookahead,on \
+                            --set wcache,off \
+                            --set wcreorder,off \
+                            --set standby,241 \
+                            $disk
     fi
 done
 
