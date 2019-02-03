346caf0e327947ab529cd74cdcb0f29026e4f225
Stanislav Graf <stanislav.graf@gmail.com>
2019-02-03 10:59:34 +0100

    Delete dnssec role

--- roles/dnssec/files/dnssec-disable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -x
-
-/usr/bin/systemctl disable dnssec-triggerd
-/usr/bin/systemctl stop dnssec-triggerd
-/usr/bin/systemctl stop unbound
-/usr/bin/sed -i 's/^dns.*/dns = dnsmasq/' /etc/NetworkManager/NetworkManager.conf
-/usr/bin/systemctl restart NetworkManager
-
--- roles/dnssec/files/dnssec-enable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-/usr/bin/systemctl enable dnssec-triggerd
-/usr/bin/systemctl start dnssec-triggerd
-/usr/bin/sed -i 's/^dns.*/dns = unbound/' /etc/NetworkManager/NetworkManager.conf
-/usr/bin/systemctl restart NetworkManager
-
