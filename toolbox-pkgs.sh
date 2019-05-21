a67af543af414e05c4f9a61fc4dcf117f8b6751c
Stanislav Graf <stanislav.graf@gmail.com>
2019-05-21 15:37:50 +0000

    Add script to configure toolbox container

---
+++ toolbox-pkgs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+sudo dnf install -y \
+    mc \
+    vim \
+    lftp \
+    htop \
+    krb5-workstation \
+    lm_sensors \
+    cyrus-sasl-gssapi \
+    openldap-clients \
+    gnome-tweaks \
+    pciutils \
+    bind-utils \
+    ansible \
+    policycoreutils
+
