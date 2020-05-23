7de5e851dd711c5864037be0cae4a2a497956ce0
Stanislav Graf <stanislav.graf@gmail.com>
2020-05-23 20:53:19 +0200

    Update hosts

--- hosts.sh
+++ hosts.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/bash
-curl -s https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts | grep -v -e '^#' -e '^$' | grep '^0\.0\.0\.0' | awk '{ print "address=/"$2"/" }' | grep -v -e 'openload.co' -e 'h\-\-\-' > fgp.conf
-curl -s https://raw.githubusercontent.com/sgraf-pub/fakenews-cz-sk/master/hosts | grep -v -e '^#' -e '^$' | grep '^0\.0\.0\.0' | awk '{ print "address=/"$2"/" }' > czf.conf
+wget --quiet --output-document=- https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts https://raw.githubusercontent.com/sgraf-pub/fakenews-cz-sk/master/hosts | grep -v -e '^#' -e '^$' -e '--' | awk '{ print "server=/"$2"/" }' | sort | uniq | sed -e "1d" > ads.conf
 
