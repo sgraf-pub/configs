fcb3d9d1b352be193fdb7b94be8125b2e7100363
Stanislav Graf <stanislav.graf@gmail.com>
2025-06-07 20:49:26 +0200

    Remove rpmfusion

--- chrome_container.sh
+++ chrome_container.sh
@@ -9,10 +9,3 @@ sudo dnf install -y vim crudini
 sudo dnf install -y fedora-workstation-repositories
 sudo dnf config-manager setopt google-chrome.enabled=1
 sudo dnf install -y google-chrome-stable
-
-# Enable HW acceleration
-rpm -q rpmfusion-nonfree-release || \
-  sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo crudini --set /etc/yum.repos.d/rpmfusion-nonfree.repo rpmfusion-nonfree includepkgs intel-media-driver
-sudo crudini --set /etc/yum.repos.d/rpmfusion-nonfree-updates.repo rpmfusion-nonfree-updates includepkgs intel-media-driver
-sudo dnf install -y intel-media-driver
