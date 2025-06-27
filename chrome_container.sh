c8bee42df8d19c7d940e2bf34ab5615d5a106cf1
Stanislav Graf <stanislav.graf@gmail.com>
2025-06-27 14:51:11 +0200

    Add intel-media-driver

--- chrome_container.sh
+++ chrome_container.sh
@@ -12,6 +12,11 @@ sudo dnf install -y google-chrome-stable
 
 # Set vulkan/vaapi
 sudo dnf install -y vulkan-tools libva-utils
+rpm -q rpmfusion-nonfree-release || \
+  sudo dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo crudini --ini-options=nospace --set /etc/yum.repos.d/rpmfusion-nonfree.repo rpmfusion-nonfree includepkgs "intel-media-driver.x86_64,rpmfusion-nonfree-release.noarch"
+sudo crudini --ini-options=nospace --set /etc/yum.repos.d/rpmfusion-nonfree-updates.repo rpmfusion-nonfree-updates includepkgs "intel-media-driver.x86_64,rpmfusion-nonfree-release.noarch"
+sudo dnf install -y intel-media-driver.x86_64
 
 # Disable 32bit packages
-sudo crudini --set /etc/dnf/dnf.conf main excludepkgs "*.i?86"
+sudo crudini --ini-options=nospace --set /etc/dnf/dnf.conf main excludepkgs "*.i?86"
