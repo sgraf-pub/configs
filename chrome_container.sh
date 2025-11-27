61e1cd8def7ee0fce61122aa450ed9512e3154cb
Stanislav Graf <stanislav.graf@gmail.com>
2025-11-27 08:49:44 +0100

    Update chrome container

--- chrome_container.sh
+++ chrome_container.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash -ex
 
 # Install Langpacks
-sudo microdnf install -y langpacks-cs langpacks-en
+sudo dnf install -y --nodocs langpacks-cs langpacks-en
 
 # Google Chrome & Firefox
-sudo microdnf install -y fedora-workstation-repositories
+sudo dnf install -y --nodocs fedora-workstation-repositories
 sudo sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/google-chrome.repo
-sudo microdnf install -y google-chrome-stable
-sudo microdnf install -y firefox
+sudo dnf install -y --nodocs google-chrome-stable
+sudo dnf install -y --nodocs firefox
 
 # Set vulkan/vaapi
 rpm -q rpmfusion-nonfree-release || \
-  sudo microdnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo microdnf install -y intel-media-driver.x86_64
+  sudo dnf install -y --nodocs https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y --nodocs intel-media-driver.x86_64
